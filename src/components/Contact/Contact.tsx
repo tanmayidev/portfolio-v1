f49951660ff5027ca65f7cc5434bac4bc4a6a574
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-29 01:29:24 +0530

    chore: refactor contact styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -11,18 +11,20 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:tanmayi.dev@gmail.com">tanmayi.dev@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/tanmayi-d-a875ba1a4/">
+            linkedin.com/in/tanmayi-d-a875ba1a4/
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/tanmayidev">github.com/tanmayidev</a>
         </li>
       </ul>
     </footer>
