36a0050f0b349ca4b9dde167d7b5131c754221c4
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-28 23:21:05 +0530

    feat: add projects section

---
+++ src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,48 @@
+import { getImageUrl } from "../../util";
+import styles from "./ProjectCard.module.css";
+
+interface IProject {
+  title: string;
+  imageSrc: string;
+  description: string;
+  skills: string[];
+  demo: string;
+  source: string;
+}
+
+interface IProjectCard {
+  project: IProject;
+}
+
+export const ProjectCard = ({
+  project: { title, imageSrc, description, skills, demo, source },
+}: IProjectCard) => {
+  return (
+    <div className={styles.container}>
+      <img
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+        <a href={demo} className={styles.link}>
+          Demo
+        </a>
+        <a href={source} className={styles.link}>
+          Source
+        </a>
+      </div>
+    </div>
+  );
+};
