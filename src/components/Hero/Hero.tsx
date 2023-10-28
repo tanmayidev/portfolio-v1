1db47b9877e1e8eb72fb825b9a653a0b70430a63
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-28 15:48:48 +0530

    feat: add hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import { getImageUrl } from "../../util";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Tanmayi</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 1+ years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:tanmayi.dev@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
